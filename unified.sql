fc1d80d4fd5c664fa7b7d21d1c02ff0e8b9de535
rschaosid <rschaosid@gmail.com>
2014-10-28 20:17:47 -0500

    more drastic changes

--- unified.sql
+++ unified.sql
@@ -7,7 +7,7 @@ CREATE FUNCTION is_valid_username(username text) RETURNS boolean AS $$
 $$ LANGUAGE sql;
 
 CREATE FUNCTION is_valid_shortname(shortname text) RETURNS boolean AS $$
-  SELECT (shortname ~ E'^\\w+$');
+  SELECT (shortname ~ E'^[\\w\\-]+$');
 $$ LANGUAGE sql;
 
 CREATE FUNCTION is_valid_simpleflake(flake bytea) RETURNS boolean AS $$
@@ -24,14 +24,14 @@ CREATE TABLE threads (
   type thread_type NOT NULL,
   thread_id smallserial PRIMARY KEY,
   category text,
-  shortname text UNIQUE NOT NULL CHECK (is_valid_shortname(shortname)),
+  shortname text NOT NULL CHECK (is_valid_shortname(shortname)),
   name text,
   description text,
   creator text CHECK (is_valid_username(creator)),
-  created timestamp,
-  scheme_name text,
-  scheme_module text
+  created timestamp
 );
+
+CREATE UNIQUE INDEX ON threads (shortname);
 
 -- permissions for `threads`
 GRANT SELECT, INSERT, UPDATE, DELETE ON threads TO counting_write;
@@ -39,12 +39,13 @@ GRANT USAGE ON threads_thread_id_seq TO counting_write;
 GRANT SELECT ON threads TO counting_read;
 
 --
--- Live thread invitations
+-- liveupdate threads and liveupdate thread invitations
 --
 
 CREATE TABLE liveupdate_threads (
   thread_id smallint NOT NULL REFERENCES threads (thread_id),
-  thread_flake bytea UNIQUE NOT NULL CHECK (is_valid_simpleflake(thread_flake))
+  liveupdate_thread_flake bytea UNIQUE NOT NULL CHECK (is_valid_simpleflake(thread_flake)),
+  PRIMARY KEY (thread_id, thread_flake)
 );
 
 -- permissions for `liveupdate_threads`
@@ -52,72 +53,104 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON liveupdate_threads TO counting_write;
 GRANT SELECT ON liveupdate_threads TO counting_read;
 
 CREATE TABLE liveupdate_thread_invitations (
-  thread smallint NOT NULL REFERENCES threads (thread_id),
+  thread_id smallint NOT NULL REFERENCES threads (thread_id),
   username text NOT NULL CHECK(is_valid_username(username)),
-  submitted timestamp DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (thread, username)
+  submitted timestamp DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE INDEX ON liveupdate_thread_invitations (thread_id, username);
 
 -- permissions for `invitations`
 GRANT SELECT, INSERT, UPDATE, DELETE ON liveupdate_thread_invitations TO counting_write;
 GRANT SELECT ON liveupdate_thread_invitations TO counting_read;
 
 --
--- Updates and comments
+-- Counts
 --
 
-CREATE TABLE valid_updates (
-  thread_id     smallint   NOT NULL REFERENCES threads (thread_id),
-  thread_flake  bytea      CHECK (is_valid_simpleflake(thread_flake)),
-  serial        integer    NOT NULL,
-  update_id     uuid,
-  author        text       CHECK (is_valid_username(author)),
-  created       timestamp,
-  body          text,
-  stricken      boolean,
-  value         integer     NOT NULL
+CREATE TABLE things (
+  thread_id                smallint   NOT NULL,
+  FOREIGN KEY (thread_id) REFERENCES threads (thread_id),
+  
+  -- comments
+  comment_link_id          integer,
+  comment_id               bigint,
+  
+  -- liveupdate updates
+  liveupdate_thread_flake  bytea,
+  liveupdate_update_id     uuid,
+  liveupdate_position      integer,
+  
+  -- common
+  author                   text       CHECK (is_valid_username(author)),
+  created                  timestamp,
+  body                     text,
+  
+  -- row does not have both comment_id and liveupdate_update_id set
+  CHECK (comment_id IS NULL OR liveupdate_update_id IS NULL),
+  
+  -- if comment_id or liveupdate_update_id is set, created is also set
+  CHECK (created IS NOT NULL OR (comment_id IS NULL AND liveupdate_update_id IS NULL)),
+  
+  -- comment_id and comment_link_id are both set or both null
+  CHECK (comment_id IS NULL = comment_link_id IS NULL),
+  
+  -- liveupdate_update_id, liveupdate_thread_flake, and liveupdate_position are all set or all null
+  CHECK (liveupdate_update_id IS NULL = liveupdate_thread_flake IS NULL
+     AND liveupdate_update_id IS NULL = liveupdate_position IS NULL),
+  
+  FOREIGN KEY (thread_id, liveupdate_thread_flake)
+    REFERENCES liveupdate_threads (thread_id, liveupdate_thread_flake) MATCH SIMPLE,
+  
+  -- for updates, author and body are set
+  CHECK ((author IS NOT NULL AND body IS NOT NULL) OR liveupdate_update_id IS NULL)
 );
 
--- Index to make count_updates fast
-CREATE INDEX ON valid_updates (thread_id, value, stricken, serial);
+CREATE TABLE queue_things (
+  FOREIGN KEY (thread_id) REFERENCES threads (thread_id),
+  
+  --comments
+  parent_id  bigint,
+  
+  --liveupdate updates
+  stricken   boolean,
+  
+  -- for updates, stricken is set
+  CHECK (stricken IS NOT NULL OR liveupdate_update_id IS NULL)
+) INHERITS (things);
 
--- Other indexes
-CREATE INDEX ON valid_updates (author);
-CREATE INDEX ON valid_updates (created);
+-- permissions for `queue_things`
+GRANT SELECT, INSERT, UPDATE, DELETE ON queue_things TO counting_write;
+GRANT SELECT ON queue_things TO counting_read;
 
--- permissions for `valid_updates`
-GRANT SELECT, INSERT, UPDATE, DELETE ON valid_updates TO counting_write;
-GRANT SELECT ON valid_updates TO counting_read;
+CREATE TABLE non_count_things (
+  FOREIGN KEY (thread_id) REFERENCES threads (thread_id),
+  
+  raw_value  integer
+) INHERITS (things);
 
--- selects a valid update for each (thread, value) combination (there is almost always only one)
-CREATE VIEW count_updates AS SELECT DISTINCT ON (thread_id, value) *
-FROM valid_updates ORDER BY thread_id, value, stricken ASC, serial ASC;
+-- permissions for `non_count_things`
+GRANT SELECT, INSERT, UPDATE, DELETE ON non_count_things TO counting_write;
+GRANT SELECT ON non_count_things TO counting_read;
 
--- permissions for `count_updates`
-GRANT SELECT ON count_updates TO counting_read;
-
-CREATE TABLE count_comments (
-  thread_id   smallint   NOT NULL REFERENCES threads (thread_id),
-  link_id     integer,
-  comment_id  bigint,
-  author      text       CHECK (is_valid_username(author)),
-  created     timestamp,
-  body        text,
-  value       integer     NOT NULL,
+CREATE TABLE count_things (
+  FOREIGN KEY (thread_id) REFERENCES threads (thread_id),
+  
+  value                    integer    NOT NULL,
+  raw_value                integer,
+  
   PRIMARY KEY (thread_id, value)
-);
+) INHERITS (things);
 
-CREATE INDEX ON count_comments (author);
-CREATE INDEX ON count_comments (created);
-CREATE INDEX ON count_comments (value);
+CREATE UNIQUE INDEX ON count_things (thread_id, value);
+CREATE UNIQUE INDEX ON count_things (thread_id, liveupdate_position)
+  WHERE (liveupdate_position IS NOT NULL);
 
--- permissions for `count_comments`
-GRANT SELECT, INSERT, UPDATE, DELETE ON count_comments TO counting_write;
-GRANT SELECT ON count_comments TO counting_read;
+CREATE INDEX ON count_things (author);
+CREATE INDEX ON count_things (created);
 
-CREATE VIEW count_things AS ((SELECT thread_id, author, created, body, value FROM count_updates)
-                   UNION ALL (SELECT thread_id, author, created, body, value FROM count_comments));
-
+-- permissions for `count_things`
+GRANT SELECT, INSERT, UPDATE, DELETE ON count_things TO counting_write;
 GRANT SELECT ON count_things TO counting_read;
 
 --
